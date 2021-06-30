b1df80688d65e632f98a658a67e85b451299dbd0
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2021-06-30 16:19:43 +0700

    Added monster spawn

--- enemy.js
+++ enemy.js
@@ -1,12 +1,21 @@
-import {canvas as cnv, context as ctx} from "./script.js";
+import {canvas as cnv, context as ctx, sprite} from "./script.js";
 let X;
 let Y;
+let additionalRandom;
 let img = new Image()
-img.src = "";
+img.src = "images/monster.png";
+img.width = 100;
+img.height = 100;
 export const enemySpawn = (numberOfEnemys) => {
-    for (let i = 0; i <= numberOfEnemys; i++){
-        X = Math.random().toFixed(2) * 100;
-        Y = Math.random().toFixed(2) * 100;
-        
+    for (let i = 1; i <= numberOfEnemys; i++){
+        additionalRandom = Math.random().toFixed(3) * 1000;
+        if (Math.random().toFixed(1) * 10 >= 5) {
+            additionalRandom *= -1;
+        }
+        X = sprite.X - 500 - additionalRandom;
+        console.log(X);
+        Y = sprite.Y - 500 - additionalRandom;
+        console.log(Y);
+        ctx.drawImage(img, X, Y);
     }
 }
